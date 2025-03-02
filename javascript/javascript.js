31aa460e5acd60f28677e8015e968500eec4f7cf
LetsBuildAFlyingDonkey <48519981+U2FUSB@users.noreply.github.com>
2025-03-02 16:51:19 +0100

    chore: made cleaner

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,17 +1,25 @@
 import { tests } from "./tests.js";
 class Calculator {
     operationDefinitions = {
-        "+": function (num1,num2) {return num1 + num2},
-        "-": function (num1,num2) {return num1 - num2},
-        "*": function (num1,num2) {return num1 * num2},
-        "/": function (num1,num2) {return num1 / num2},
-    }
+        "+": function (num1, num2) {
+            return num1 + num2;
+        },
+        "-": function (num1, num2) {
+            return num1 - num2;
+        },
+        "*": function (num1, num2) {
+            return num1 * num2;
+        },
+        "/": function (num1, num2) {
+            return num1 / num2;
+        },
+    };
     operate(operation) {
-        const num1 = operation[0]
-        const num2 = operation[2]
-        const operator = operation[1]
+        const num1 = operation[0];
+        const num2 = operation[2];
+        const operator = operation[1];
 
-        return this.operationDefinitions[operator](num1,num2);
+        return this.operationDefinitions[operator](num1, num2);
     }
 }
 
