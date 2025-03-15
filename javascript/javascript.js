9a7c4abf34ab1482b6a7cd1b6c70e522b0d6c8bf
LetsBuildAFlyingDonkey <48519981+U2FUSB@users.noreply.github.com>
2025-03-15 22:59:39 +0100

    refactor: Removed current calculator operation for alternative algorithm
    Current operation leads to complex conditions at wrong places and bad maintenance.
    Added pseudocode algorithm which should work more maintanable when
    implemented.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -4,34 +4,37 @@ class Calculator {
         this.setDisplayableButtons(this.numberButtons);
         this.setOperationButtons(this.operationButtons);
     }
-    allButtons = this.getButtons(".buttons");
     numberButtons = this.getButtons(".numbers");
     operationButtons = this.getButtons(".operations");
     displayElement = document.querySelector(".display .text-output");
     operationDefinitions = {
         "+": function (num1, num2) {
-            return num1 + num2;
+            return +num1 + +num2;
         },
         "-": function (num1, num2) {
-            return num1 - num2;
+            return +num1 - +num2;
         },
         "*": function (num1, num2) {
-            return num1 * num2;
+            return +num1 * +num2;
         },
         "/": function (num1, num2) {
-            return num1 / num2;
+            if (+num2 === 0) {
+                return "Error";
+            }
+            return +num1 / +num2;
         },
     };
     savedInput = [];
-    cleanDisplayOnNextInput = false;
+    cleanDisplayOnNextNumberInput = false;
+    justEnteredAnOperator = false;
     getButtons(cssParentClass) {
         const buttons = [];
         getLeafElements(document.querySelectorAll(cssParentClass), buttons);
         return buttons;
     }
     operate(operation) {
-        const num1 = Number(operation[0]);
-        const num2 = Number(operation[2]);
+        const num1 = operation[0];
+        const num2 = operation[2];
         const operator = operation[1];
 
         return this.operationDefinitions[operator](num1, num2);
@@ -39,9 +42,10 @@ class Calculator {
     setDisplayableButtons(buttons) {
         buttons.forEach((button) => {
             button.addEventListener("click", () => {
-                if (this.cleanDisplayOnNextInput) {
+                this.justEnteredAnOperator = false;
+                if (this.cleanDisplayOnNextNumberInput) {
                     this.displayElement.textContent = "";
-                    this.cleanDisplayOnNextInput = false;
+                    this.cleanDisplayOnNextNumberInput = false;
                 }
                 this.displayElement.textContent += button.textContent;
             });
@@ -50,31 +54,31 @@ class Calculator {
     setOperationButtons(buttons) {
         buttons.forEach((button) => {
             button.addEventListener("click", () => {
-                this.savedInput.push(this.displayElement.textContent);
-                this.savedInput.push(button.textContent);
-                this.cleanDisplayOnNextInput = true;
-                console.log(this.savedInput);
-                if (this.savedInput.length === 4) {
-                    const [num1, operator, num2] = this.savedInput;
-                    const operationResult = this.operate([
-                        num1,
-                        operator,
-                        num2,
-                    ]).toString();
-                    this.displayElement.textContent = operationResult;
-                    this.cleanDisplayOnNextInput = true;
-                    if (button.textContent === "=") {
-                        this.savedInput.splice(0);
-                    } else {
-                        this.savedInput.splice(0, 3);
-                        this.savedInput.unshift(operationResult);
-                    }
-                }
-                if (this.savedInput.length > 4) {
-                    this.displayElement.textContent = "Error";
-                    this.savedInput.splice(0);
-                    this.cleanDisplayOnNextInput = true;
-                }
+                this.cleanDisplayOnNextNumberInput = true;
+                // IF (
+                    // display content !== 'Error' && 
+                    // display content !== '' &&
+                    // display content not more then one '.'
+                    // )
+                    // case: 0 length array
+                        // IF (operator !== '=')
+                            // Add display-content and operator to array
+                            // set this.justEnteredAnOperator = true
+                    // case: 2 length array
+                        // IF (
+                            // this.justEnteredAnOperator === true &&
+                            // operator !== '='
+                            // )
+                            // replace last operator in Array with new one
+                        // IF (this.justEnteredAnOperator === false)
+                            // Add display-content to array
+                            // run operation of first 3 elements (should never be more then 3)
+                            // overwrite display with result
+                            // clean array
+                            // IF (operator !== '=')
+                                // Add display-content and operator to array
+                                // set this.justEnteredAnOperator = true
+
             });
         });
     }
