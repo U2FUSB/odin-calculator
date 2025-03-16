b938edbb8ade4fecfb79cd5a2586d4deb1a5a181
LetsBuildAFlyingDonkey <48519981+U2FUSB@users.noreply.github.com>
2025-03-16 23:46:45 +0100

    feat: Reimplemented main operation

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -28,6 +28,15 @@ class Calculator {
     cleanDisplayOnNextNumberInput = false;
     justEnteredAnOperator = false;
     getButtons(cssParentClass) {
+        function getLeafElements(elements, leafElementContainer) {
+            Array(...elements).forEach((element) => {
+                if (element.children.length !== 0) {
+                    getLeafElements(element.children, leafElementContainer);
+                } else {
+                    leafElementContainer.push(element);
+                }
+            });
+        }
         const buttons = [];
         getLeafElements(document.querySelectorAll(cssParentClass), buttons);
         return buttons;
@@ -37,7 +46,54 @@ class Calculator {
         const num2 = operation[2];
         const operator = operation[1];
 
-        return this.operationDefinitions[operator](num1, num2);
+        return this.operationDefinitions[operator](num1, num2).toString();
+    }
+    handleInput(button) {
+        if (
+            this.displayElement.textContent !== "Error" &&
+            this.displayElement.textContent !== "" &&
+            this.displayElement.textContent.split(".").length - 1 <= 1
+        ) {
+            switch (this.savedInput.length) {
+                case 0:
+                    console.log(0);
+                    if (button.textContent !== "=") {
+                        this.savedInput.push(
+                            this.displayElement.textContent,
+                            button.textContent
+                        );
+                        this.justEnteredAnOperator = true;
+                    }
+                    break;
+                case 2:
+                    if (
+                        this.justEnteredAnOperator === true &&
+                        button.textContent !== "="
+                    ) {
+                        this.savedInput[this.savedInput.length - 1] =
+                            button.textContent;
+                    }
+                    if (this.justEnteredAnOperator === false) {
+                        this.savedInput.push(this.displayElement.textContent);
+                        const [num1, operator, num2] = this.savedInput;
+                        const operation = [num1, operator, num2];
+                        const operationResult = this.operate(operation);
+                        this.displayElement.textContent = operationResult;
+                        this.savedInput.splice(0);
+                        if (button.textContent !== "=") {
+                            this.savedInput.push(
+                                this.displayElement.textContent,
+                                button.textContent
+                            );
+                            this.justEnteredAnOperator === true;
+                        }
+                    }
+                    break;
+                default:
+                    this.displayElement.textContent = "Error";
+                    break;
+            }
+        }
     }
     setDisplayableButtons(buttons) {
         buttons.forEach((button) => {
@@ -55,45 +111,13 @@ class Calculator {
         buttons.forEach((button) => {
             button.addEventListener("click", () => {
                 this.cleanDisplayOnNextNumberInput = true;
-                // IF (
-                    // display content !== 'Error' && 
-                    // display content !== '' &&
-                    // display content not more then one '.'
-                    // )
-                    // case: 0 length array
-                        // IF (operator !== '=')
-                            // Add display-content and operator to array
-                            // set this.justEnteredAnOperator = true
-                    // case: 2 length array
-                        // IF (
-                            // this.justEnteredAnOperator === true &&
-                            // operator !== '='
-                            // )
-                            // replace last operator in Array with new one
-                        // IF (this.justEnteredAnOperator === false)
-                            // Add display-content to array
-                            // run operation of first 3 elements (should never be more then 3)
-                            // overwrite display with result
-                            // clean array
-                            // IF (operator !== '=')
-                                // Add display-content and operator to array
-                                // set this.justEnteredAnOperator = true
-
+                this.handleInput(button);
             });
         });
     }
 }
-function getLeafElements(elements, leafElementContainer) {
-    Array(...elements).forEach((element) => {
-        if (element.children.length !== 0) {
-            getLeafElements(element.children, leafElementContainer);
-        } else {
-            leafElementContainer.push(element);
-        }
-    });
-}
 const calculator = new Calculator();
-const { allButtons, numberButtons, operationButtons } = calculator;
+const { numberButtons, operationButtons } = calculator;
 console.groupCollapsed("Calculator contents");
 console.table(Object.getOwnPropertyDescriptors(calculator));
 console.group("numbers");
